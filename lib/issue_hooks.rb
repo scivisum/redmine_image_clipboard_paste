44cf2c1f9071b63217dba9f02fbceca2755fbb89
Ben Walker <ben.walker@scivisum.co.uk>
2014-06-04 10:42:48 +0100

    Add the js to all pages so it works on the wiki as well as issues

--- lib/issue_hooks.rb
+++ lib/issue_hooks.rb
@@ -1,3 +1,5 @@
 class RedmineImageClipboardPasteHook < Redmine::Hook::ViewListener
-  render_on :view_issues_form_details_bottom, :partial => 'imagepaste'
+  def view_layouts_base_html_head(context)
+    javascript_include_tag 'image_paste.js', :plugin => 'redmine_image_clipboard_paste'
+  end
 end
